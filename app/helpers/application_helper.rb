c2a628205cc6d898a8381f3b60ff870d6d9a3658
sdrozzy <sdrozzy@gmail.com>
2015-01-08 13:09:18 -0800

    product hunt style scaffolding setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Seeder"
+    base_title = "Track Sense"
     if page_title.empty?
       base_title
     else
-      "#{page_title} | #{base_title}"
+      "#{page_title}"
     end
   end
 
