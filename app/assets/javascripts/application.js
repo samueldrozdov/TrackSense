48dd6090f7a2002b3f251fb77762aa01a394b5bd
Andrew Brandt <abrandt@makeschool.com>
2015-01-09 12:19:06 -0800

    Add basic player widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+(function(){
+  var widgetIframe = document.getElementById('sc-widget'),
+  widget       = SC.Widget(widgetIframe);
+
+  widget.bind(SC.Widget.Events.READY, function() {
+    widget.bind(SC.Widget.Events.PLAY, function() {
+      // get information about currently playing sound
+      widget.getCurrentSound(function(currentSound) {
+        console.log('sound ' + currentSound.get('') + 'began to play');
+      });
+    });
+    // get current level of volume
+    widget.getVolume(function(volume) {
+      console.log('current volume value is ' + volume);
+    });
+    // set new volume level
+    widget.setVolume(50);
+    // get the value of the current position
+  });
+
+}());
