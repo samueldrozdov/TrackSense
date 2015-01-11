2b68d55e3ca36f4aabf380b83a32ef85cbf94262
Morgan Chen <morganchen12@gmail.com>
2015-01-11 15:28:17 -0800

    added submissions controller and partial

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,12 +1,19 @@
 class SubmissionsController < ApplicationController
 
   def create
+    @submission = current_user.submissions.build
+    if @submission.save(params[:submission])
+      flash[:success] = "Post successful"
+    else
+      # error submitting
+      flash[:danger] = "Song has already been posted"
+    end
+    redirect_to root_url
   end
 
   def destroy
-  end
-
-  def show
+    # actually implement this later
+    redirect_to root_url
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111222840) do
+ActiveRecord::Schema.define(version: 20150111232359) do
 
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20150111222840) do
     t.integer  "track_length"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "name"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
