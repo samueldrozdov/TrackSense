d6fc81b89083bda039bd067d314c7f14b45e64cd
Morgan Chen <morganchen12@gmail.com>
2015-01-13 11:13:53 -0800

    added vote uniqueness validation and model tests

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true, dependent: :destroy
 
   validates :votable_id, presence: true
-  validates :user_id, presence:true
+  validates :user_id, presence: true, uniqueness: { scope: [:votable_type, :votable_id] }
   validates :votable_type, presence: true
 end
