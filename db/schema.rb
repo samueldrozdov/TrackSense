ba5ec67e427a98ac0c2da59e1a9da9ed69c6bf8c
sdrozzy <sdrozzy@gmail.com>
2015-01-13 14:31:22 -0800

    default profile image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112222742) do
+ActiveRecord::Schema.define(version: 20150113214011) do
 
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150112222742) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "remember_digest"
+    t.string   "image"
   end
 
   create_table "votes", force: :cascade do |t|
