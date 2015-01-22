29aad0ccad75563ed17cf108d824eba40503ecbd
Morgan Chen <morganchen12@gmail.com>
2015-01-22 11:31:59 -0800

    made signup/login async

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,13 +4,9 @@ class VotesController < ApplicationController
     if logged_in?
       @vote = Vote.find_by(user_id: current_user.id, votable_id: params[:votable_id])
 
-
       if @vote
-
         @vote.destroy
-
       else
-
         @vote = current_user.votes.build(user_id: params[:user_id],
                                       votable_id: params[:votable_id],
                                     votable_type: params[:votable_type])
