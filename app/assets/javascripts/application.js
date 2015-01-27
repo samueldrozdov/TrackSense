50d072cd155ede5c63951cdd636dfb73147cff7a
sdrozzy <sdrozzy@gmail.com>
2015-01-27 10:52:16 -0800

    scroll away header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,47 @@
 songArray = []
 currentlyPlaying = 0;
 
+// Hide header on scroll down, show on scroll up
+var didScroll;
+var lastScrollTop = 0;
+var delta = 70;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+  didScroll = true;
+});
+
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  var st = $(this).scrollTop();
+
+  // Make sure they scroll more than delta
+  if(Math.abs(lastScrollTop - st) <= delta)
+    return;
+
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight){
+    // Scroll Down
+    $('header').removeClass('nav-down').addClass('nav-up');
+  } else {
+    // Scroll Up
+    if(st + $(window).height() < $(document).height()) {
+      $('header').removeClass('nav-up').addClass('nav-down');
+    }
+  }
+
+  lastScrollTop = st;
+}
+
+
+
 var mapPlayOnClick = function() {
   if ( $(this).hasClass("playing glyphicon-play")) {
     widget.play();
