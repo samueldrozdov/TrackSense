ec2e7a14b0879426acc6639ff254e0b29b60d9b9
Morgan Chen <morganchen12@gmail.com>
2015-01-28 17:04:20 -0800

    validations in relationship

--- app/models/tag_relationship.rb
+++ app/models/tag_relationship.rb
@@ -1,4 +1,7 @@
 class TagRelationship < ActiveRecord::Base
   belongs_to :tag, class_name: "Tag"
   belongs_to :tagged, class_name: "Submission"
+
+  validates :tag_id, presence: true
+  validates :tagged_id, presence: true
 end
