b6ff118c266f6143a024ff6d0da6b42b94599c34
sdrozzy <sdrozzy@gmail.com>
2015-02-01 12:29:30 -0800

    pulling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
 
-  def image_for(user, string)
+  def image_for(user)
     if @user.image.nil?
-      image_tag("default-profile-image-1.png", class: string)
+      image_tag("default-profile-image-1.png")
     else
       @user.image.url
     end
