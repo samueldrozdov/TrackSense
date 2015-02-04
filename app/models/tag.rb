61def43b8d3d94c0c3123973d245e45e0bdf586d
sdrozzy <sdrozzy@gmail.com>
2015-02-04 14:20:28 -0800

    new header for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   VALID_TAG_REGEX = /\A(\w)+\Z/i
 
-  validates :name, presence: true, length: { maximum: 50 }, 
+  validates :name, presence: true, length: { maximum: 50 },
                    uniqueness: { case_sensitive: false },
                    format: { with: VALID_TAG_REGEX}
 
@@ -10,4 +10,6 @@ class Tag < ActiveRecord::Base
                                   dependent: :destroy
   # taggeds is awkward but idk how to make it better
   has_many :submissions, through: :active_relationships, source: :tagged
+
+  has_many :users, -> { uniq }, through: :submissions, source: :user
 end
